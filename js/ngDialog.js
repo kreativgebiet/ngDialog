eb4bf33425df7ed673b8f654415b5c9fe2d23666
Dominik Schmidt <evolveeee@gmail.com>
2016-06-21 21:18:39 +0200

    remove commonjs

--- js/ngDialog.js
+++ js/ngDialog.js
@@ -5,21 +5,7 @@
  */
 
 (function (root, factory) {
-    if (typeof module !== 'undefined' && module.exports) {
-        // CommonJS
-        if (typeof angular === 'undefined') {
-            factory(require('angular'));
-        } else {
-            factory(angular);
-        }
-        module.exports = 'ngDialog';
-    } else if (typeof define === 'function' && define.amd) {
-        // AMD
-        define(['angular'], factory);
-    } else {
-        // Global Variables
-        factory(root.angular);
-    }
+    factory(root.angular);
 }(this, function (angular) {
     'use strict';
 
